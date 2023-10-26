0f26d6b4e158dc6b94a840a09a2080f9ee13c37e
Fujie Huang <jie.huangfu@gmail.com>
2023-10-26 11:25:28 -0500

    ..

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='saicinpainting',
+    version='0.1.0',
+    packages=find_packages(include=['saicinpainting', 'saicinpainting.*'])
+)
